f45141fbba09662985f91b9a6255e866dd7fc3fd
tyn <tuyenpham1104.dev@gmail.com>
2024-03-17 03:40:33 +0700

    nua ne

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,14 +8,10 @@ import styles from './home.module.css'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
-import { useEffect } from 'react'
 
 export interface HomeProps {}
 
 export default function Home() {
-  useEffect(() => {
-    document.body.style.height = 'auto'
-  }, [])
   return (
     <div className={styles.home_wrapper}>
       <div className={styles.booth_wrapper}>
